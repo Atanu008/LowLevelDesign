60fd154d953907d4bd3258847068e9c3b63d2fd8
Atanu <atanu2destiny@gmail.com>
2022-08-06 15:38:24 +0530

    Rate Limiting

---
+++ RateLimiterSystem/src/org/ratelimiter/SlidingWindowCounter.java
@@ -0,0 +1,38 @@
+package org.ratelimiter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+//https://www.educative.io/courses/grokking-modern-system-design-software-engineers-managers/q2zzAmqQOy3
+public class SlidingWindowCounter extends RateLimiter{
+
+    Map<Long, AtomicInteger> windows;
+
+    public SlidingWindowCounter(int maxRequestPerSec) {
+        super(maxRequestPerSec);
+        windows = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public boolean allowRequest() {
+
+        long currentTime = System.currentTimeMillis();
+        long currentWindow = currentTime / 1000; //window in second;
+        long previousWindow = currentWindow - 1;
+
+        AtomicInteger previousWindowCount= windows.get(previousWindow); // get previous window hit
+        //No previous window
+        if(previousWindowCount == null){
+            return windows.get(currentWindow).getAndIncrement() <= MAX_TOKEN;
+        }
+
+        long currentWindowOverLapTime = currentTime - (currentWindow * 1000);
+        double previousWindowOverLapPercentage = (1000 - currentWindowOverLapTime) / 1000.0;
+
+        long requestCountInCurrentSlidingWindow = (long)(previousWindowCount.get() * previousWindowOverLapPercentage
+                                                  + windows.get(currentWindow).getAndIncrement());
+
+        return requestCountInCurrentSlidingWindow <= MAX_TOKEN;
+    }
+}
--- RateLimiterSystem/src/org/ratelimiter/test/Main.java
+++ RateLimiterSystem/src/org/ratelimiter/test/Main.java
@@ -6,6 +6,25 @@ public class Main {
 
         long windowKey = (long) (System.currentTimeMillis() / 1e3);
 
-        System.out.println(windowKey);
+        //System.out.println(windowKey);
+
+        long curTime = System.currentTimeMillis();
+        long curWindowKey = curTime / 1000;
+
+        System.out.println(curTime +" "+ curWindowKey);
+
+        long overLapTime = curTime - (curWindowKey * 1000);
+        System.out.println(overLapTime);
+
+        double percentage = (1000 - overLapTime) / 1000.0;
+        System.out.println(percentage);
+
+        //88 * 0.134 + 12;
+
+        //1658119077506
+
+
+
+
     }
 }
