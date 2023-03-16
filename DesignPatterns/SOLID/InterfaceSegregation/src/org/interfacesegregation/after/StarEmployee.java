1fc019d2de71becac7e8571b7d8a31108c0c1d72
Atanu <atanu2destiny@gmail.com>
2023-03-16 23:30:43 +0530

    SOLID

---
+++ DesignPatterns/SOLID/InterfaceSegregation/src/org/interfacesegregation/after/StarEmployee.java
@@ -0,0 +1,23 @@
+package org.interfacesegregation.after;
+
+public class StarEmployee implements WaiterInterface, ChefInterface{
+    @Override
+    public void cookFood() {
+
+    }
+
+    @Override
+    public void decideMenu() {
+
+    }
+
+    @Override
+    public void serverCustomers() {
+
+    }
+
+    @Override
+    public void takeOrders() {
+
+    }
+}
---
+++ DesignPatterns/SOLID/InterfaceSegregation/src/org/interfacesegregation/after/Waiter.java
@@ -0,0 +1,13 @@
+package org.interfacesegregation.after;
+
+public class Waiter implements WaiterInterface{
+    @Override
+    public void serverCustomers() {
+
+    }
+
+    @Override
+    public void takeOrders() {
+
+    }
+}
